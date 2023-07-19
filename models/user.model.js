da2a6952a6ec7e677c9b1d367c62ed09fceb81ef
SujalDhiman <sujaldhiman2003@gmail.com>
2023-07-20 00:58:42 +0530

    User Model

---
+++ models/user.model.js
@@ -0,0 +1,85 @@
+const mongoose=require("mongoose")
+const validator=require("validator")
+const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
+const crypto=require("crypto")
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        unique:[true,"Name Already Taken"],
+        required:[true,"This field is required"]
+    },
+    email:{
+        type:String,
+        required:[true,"This field is required"],
+        validate:[validator.isEmail,"Please enter email in correct format"]
+    },
+    password:{
+        type:String,
+        unique:[true,"Enter new password"],
+        minLength:[6,"Password should be atleast 6 characters"],
+        required:[true,"This field is required"]
+    },
+    role:{
+        type:String,
+        default:"user",
+        enum:["user","admin"]
+    },
+    photo:{
+        id:{
+            type:String,
+            required:[true,"This is Required "]
+        },
+        secure_url:{
+            type:String,
+            requierd:[true,"This is Required "]
+        }
+    },
+    forgotPasswordToken:String,
+    forgotPasswordTokenExpiry:Date
+})
+
+//Hashing the password before save
+userSchema.pre("save",async function (next){
+    if(!this.isModified(password))
+    {
+        next()
+    }
+    this.password=await bcrypt.hash(this.password,10)
+})
+
+//validating Password
+userSchema.methods.validatePassword=async function (gotPassword){
+    try{
+    return await bcrypt.compare(this.password,gotPassword)
+    }
+    catch(error)
+    {
+        console.log("An error occured")
+    }
+}
+
+//generating a jwt token
+userSchema.methods.generateJWToken=function (){
+    const token=jwt.sign({
+        id:this._id,
+        email:this.email
+    },process.env.JWT_SECRET,{
+        expiresIn:process.env.JWT_EXPIRYDATE
+    })
+
+    return token
+}
+
+//creating a forgot password token
+userSchema.methods.getForgotPasswordToken=function (){
+    //generating a random long string
+    const forgotToken=crypto.randomBytes(16).toString("hex")
+
+    this.forgotPasswordToken=forgotToken
+    this.forgotPasswordTokenExpiry=Date.now() + 10*60*1000 //valid for 10 mins only
+    return forgotToken
+}
+
+module.exports=mongoose.model("User",userSchema)
