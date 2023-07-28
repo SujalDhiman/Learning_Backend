c76ea20183a92d1bbea6a1357af6e918409bd3bc
SujalDhiman <sujaldhiman2003@gmail.com>
2023-07-28 23:43:35 +0530

    Payment Integrated

---
+++ routers/payment.js
@@ -0,0 +1,12 @@
+const express=require("express")
+const router=express.Router()
+const {isLoggedIn}=require("../middlewares/user")
+
+const {sendStripeKey,captureStripePayment}=require("../controllers/paymentController")
+
+
+router.get("/stripekey",isLoggedIn,sendStripeKey)
+router.post("/stripePayment".isLoggedIn,captureStripePayment)
+
+
+module.exports=router
